a983adced4cb40b00497f12e6af5f132634bca6f
Gabriel Kenji <134003510+Jackoki@users.noreply.github.com>
2023-11-13 16:06:22 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,8 +46,10 @@ export default function Home() {
   }
 
   function idProximaPergunta() {
+    if(questao) {
       const proximoIndice = idsDasQuestoes.indexOf(questao.id) + 1
       return idsDasQuestoes[proximoIndice]
+    }
   }
 
   function irPraProximoPasso() {
