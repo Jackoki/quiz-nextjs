92645a3bcd47aeb8caa755aa7d4759797e4b3dd2
Gabriel Kenji <134003510+Jackoki@users.noreply.github.com>
2023-11-13 16:25:01 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Questionario from '../components/Questionario'
 import { useRouter } from 'next/router'
 
 
-const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = 'http://quiz-nextjs-six.vercel.app/api'
 
 export default function Home() {
   const router = useRouter()
